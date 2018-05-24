31b0639eae5054d348c231e1e66f25eb03afcc24
Dmitry Chertok <chertokdmitry@yandex.ru>
2018-05-24 16:41:31 +0300

    V24

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Braingames' => array($baseDir . '/bin'),
+    'Braingames\\' => array($baseDir . '/bin'),
 );
